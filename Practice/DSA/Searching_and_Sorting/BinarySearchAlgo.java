dd7a1cc19441b8e415c22b2a07b802d8c8ba205d
Restyled.io <commits@restyled.io>
2023-08-11 07:20:36 +0000

    Restyled by clang-format

--- Practice/DSA/Searching_and_Sorting/BinarySearchAlgo.java
+++ Practice/DSA/Searching_and_Sorting/BinarySearchAlgo.java
@@ -1,83 +1,60 @@
 package Practice.DSA.Searching_and_Sorting;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class BinarySearchAlgo {
 
-    public static int binaryS(int arr[],int target)
-    {
-        int low=0;
-        int high =arr.length-1;
+  public static int binaryS(int arr[], int target) {
+    int low = 0;
+    int high = arr.length - 1;
 
-        while(low<=high)
-        {
-            int mid = low+(high-low)/2;
-            if(arr[mid]==target)
-            {
-                return mid;
-            }
-            else if(arr[mid]<high)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-            
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] == target) {
+        return mid;
+      } else if (arr[mid] < high) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter size of array: ");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    System.out.println("Entre elements : ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (arr[i] > arr[j]) {
+          int t = arr[i];
+          arr[i] = arr[j];
+          arr[j] = t;
         }
-        return 0;
+      }
+      // int a1[] = new int[n];
+      for (i = 0; i < n; i++) {
+        System.out.println(arr[i]);
+      }
     }
 
+    System.out.println("Find the element ");
 
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Entre elements : ");
-        for(int i=0;i<n;i++)
-        {
-            arr[i] = sc.nextInt();
-            
-        }
-       for(int i=0;i<n;i++)
-            {
-            for(int j=i+1;j<n;j++)
-        {
-        if(arr[i]>arr[j]){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-        }
+    int ta = sc.nextInt();
+
+    int res = binaryS(arr, ta);
+
+    if (res == 0) {
+      System.out.println("Not found");
+    } else {
+      System.out.println("Element founded " + res);
     }
-    // int a1[] = new int[n];
-    for( i=0;i<n;i++)
-    {
-      System.out.println(arr[i]);
-
-    }
-
-
-}
-
-        System.out.println("Find the element ");
-        
-            int ta = sc.nextInt();
-        
-        
-        
-        int res = binaryS(arr, ta);
-
-        if(res==0)
-        {
-            System.out.println("Not found");
-        }
-        else
-        {
-            System.out.println("Element founded "+ res);
-        }
-
-    }
+  }
 }
--- Timepass/Dem.java
+++ Timepass/Dem.java
@@ -1,40 +1,32 @@
 import java.util.Scanner;
 
-class Dem{
-public static void main(String args[])
-{
-// for(;;)
-// {
-// System.out.print("Hacked  ");
-// }
-// while(true)
-// {
-//     System.out.println("Wer");
-// }
-Scanner sc = new Scanner(System.in);
-int a[]= new int[6];
-for(int i=0;i<5;i++)
-{
+class Dem {
+  public static void main(String args[]) {
+    // for(;;)
+    // {
+    // System.out.print("Hacked  ");
+    // }
+    // while(true)
+    // {
+    //     System.out.println("Wer");
+    // }
+    Scanner sc = new Scanner(System.in);
+    int a[] = new int[6];
+    for (int i = 0; i < 5; i++) {
 
-    a[i]= sc.nextInt();
-}
-for(int i=0;i<5;i++)
-{
-    for(int j=i+1;j<5;j++)
-    {
-        if(a[i]>a[j]){
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-        }
+      a[i] = sc.nextInt();
     }
-
+    for (int i = 0; i < 5; i++) {
+      for (int j = i + 1; j < 5; j++) {
+        if (a[i] > a[j]) {
+          int t = a[i];
+          a[i] = a[j];
+          a[j] = t;
+        }
+      }
+    }
+    for (int i = 0; i < 5; i++) {
+      System.out.print(a[i] + " ");
+    }
+  }
 }
-for(int i=0;i<5;i++)
-{
-    System.out.print(a[i]+" ");
-
-}
-}
-}
-
