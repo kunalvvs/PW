ab261f852f1cd0dc896c6f67c0825caa10d40428
kunalvvs <kunalvvs@gmail.com>
2025-03-07 20:00:17 +0530

    Fix syntax error in if statement in K.java by replacing invalid variable with valid integer

--- Practice/Pattern/K.java
+++ Practice/Pattern/K.java
@@ -3,11 +3,12 @@ package Practice.Pattern;
 public class K {
     public static void main(String[] args) {
         int n=10;
+
         for(int i=1;i<n;i++)
         {
             for(int j=1;j<n;j++)
             {
-              if( i==1 || j==ghn)
+              if( i==1 || j==n)
               {
                 System.out.println("*");
               }
