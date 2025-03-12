b9088e6ce610046c886262e25b1e3694cc3cedfb
kunalvvs <kunalvvs@gmail.com>
2025-03-12 23:01:50 +0530

    LEAP

--- Practice/TCS/Numbers/LeapYear.java
+++ Practice/TCS/Numbers/LeapYear.java
@@ -2,7 +2,7 @@ package Practice.TCS.Numbers;
 
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 2000;
+        int year = 2005;
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
