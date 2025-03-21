06d3f966d396d4995db29fb4099887b3ec33f977
kunalvvs <kunalvvs@gmail.com>
2025-03-21 22:57:21 +0530

    Refactor K.java for improved formatting and add F.java and Triangle.java to generate specific patterns

--- Practice/Pattern/F.java
+++ Practice/Pattern/F.java
@@ -1,12 +1,12 @@
 package Practice.Pattern;
 
-public class K {
+public class F {
     public static void main(String[] args) {
         int n = 10;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (j == 0 || i + j == n / 2 || i - j == n / 2) {
+                if (i == 0 || i == n / 2 || j == 0) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
---
+++ Practice/Pattern/Triangle.java
@@ -0,0 +1,14 @@
+package Practice.Pattern;
+
+public class Triangle {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
