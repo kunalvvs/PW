13d465fba628741d23ad85d70aab0ef9a9f965bb
kunalvvs <kunalvvs@gmail.com>
2025-03-23 18:04:46 +0530

    Add comments to clarify upper and lower parts of the diamond pattern in Diamond.java

--- Practice/Pattern/Diamond.java
+++ Practice/Pattern/Diamond.java
@@ -4,6 +4,7 @@ public class Diamond {
     public static void main(String[] args) {
         int n = 5;
 
+        // Upper part of the diamond
         for (int i = 0; i < n; i++) {
             for (int j = n - i; j > 1; j--) {
                 System.out.print(" ");
@@ -13,8 +14,10 @@ public class Diamond {
             }
             System.out.println();
         }
+
+        // Lower part of the diamond
         for (int i = n - 1; i > 0; i--) {
-            for (int j = n - i; j > 1; j--) {
+            for (int j = n - i; j > 0; j--) {
                 System.out.print(" ");
             }
             for (int j = 0; j < i; j++) {
