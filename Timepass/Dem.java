42052bdefb6e07d1c2dc1334756a923f424bc51e
kunalvvs <kunalvvs@gmail.com>
2025-04-02 22:57:46 +0530

    Fix indentation and formatting in Dem.java

--- Timepass/Dem.java
+++ Timepass/Dem.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 class Dem{
 public static void main(String args[])
-{
+  {
 for(;;)
-{
-System.out.print("Hacked  ");
+  {
+
+    System.out.print("Hacked  ");
 }
 //12th aug 23
 // while(true)
